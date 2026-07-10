9db88d0a96eac9e4c3bb6b9543919c0ba259e789
km <kanishthamaheshwari@gmail.com>
2026-07-10 17:28:17 +0530

    home page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,24 @@ import { useState } from 'react'
 import Login from './pages/Login.jsx'
 import{BrowserRouter , Routes , Route} from "react-router-dom";
 import Signup from "./pages/Signup.jsx";
+import Navbar from './components/Navbar.jsx'
+import Discover from "./pages/Discover";
+import Itineraries from "./pages/Itineraries";
+import Destinations from "./pages/Destinations";
+import Guides from "./pages/Guides";
 
 function App() {
   const[darkMode , setdarkMode]= useState(false);
   return(<>
   <BrowserRouter>
+   <Navbar/>
   <Routes>
     <Route path="/" element={<Login darkMode={darkMode} setdarkMode={setdarkMode}/>}></Route>
     <Route path="/signup" element={<Signup darkMode={darkMode} setdarkMode={setdarkMode} />}></Route>
+    <Route path="/discover" element={<Discover />} />
+        <Route path="/itineraries" element={<Itineraries />} />
+        <Route path="/destinations" element={<Destinations />} />
+        <Route path="/guides" element={<Guides />} />
   </Routes>
   </BrowserRouter>
   </>);
---
+++ frontend/src/pages/Guides.jsx
@@ -0,0 +1,5 @@
+function Guides() {
+  return <h1>Guides Page</h1>;
+}
+
+export default Guides;
