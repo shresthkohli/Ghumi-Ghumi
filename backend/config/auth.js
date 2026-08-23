dc36fa3f850bfade8eff28d8b7b9fc1e13795dca
Geet <geetlahoty@gmail.com>
2026-08-24 04:17:24 +0530

    cookie changes

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,6 +1,6 @@
 export const COOKIE_OPTIONS = {
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     secure: process.env.NODE_ENV === "production",
     maxAge: 7 * 24 * 60 * 60 * 1000
 };
