22e76a78fdc3ec674ffa56359df3e7c2d225a9cf
Geet <geetlahoty@gmail.com>
2026-08-24 03:25:41 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 3000;
 //accepting cross site cookies
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         credentials: true
     })
 );
